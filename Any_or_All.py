b0b8f4155998e7a0c7ec17fb71bd3741756e2d42
szygp <92905220+szygp@users.noreply.github.com>
2022-08-06 19:32:10 +0800

    Any_or_All

--- Any_or_All.py
+++ Any_or_All.py
@@ -1,16 +1,17 @@
+复杂版
 n = int(input())
 m = input()
 list_number = list(map(str,m.split()))
 list_number_int = list(map(int,m.split()))
-result1 = any(list_number[i] in ['1','2','3','4','5','6','7','8','9'] for i in range(n))
+
 result2 = all(list_number[i]==list_number[i][::-1] for i in range(n))
 result3 = all(list_number_int[i]> 0 for i in range(n))
-def result_true():
-    if result1 or result2:
-        return True
-    else:
-        return False
-if result3 and result_true():
+
+if result3 and result2:
     print(True)
 else:
     print(False)
+
+简化版
+m,n = input(), input().split()
+print (all(int(n[i]) > 0 for i in range(int(m))) and any(j == j[::-1] for j in n))
