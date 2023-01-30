585b99be051b56d7ea934cf9e4c6ed3fa9d991ba
szygp <92905220+szygp@users.noreply.github.com>
2023-01-30 12:12:38 +0800

    Add files via upload

--- Maximize_It.py
+++ Maximize_It.py
@@ -6,8 +6,24 @@ my_list = []
 
 for i in range(int(K)):
     x = list(map(int, input().split()))
-    del (x[0])
-    my_list.append(x)
+    from itertools import product
+
+    K, M = input().split()
+    M = int(M)
+    my_list = []
+
+    for i in range(int(K)):
+        x = list(map(int, input().split()))
+        my_list.append(x[1:])
+
+    list_product = list(product(*my_list))
+
+    max_number = 0
+    for j in list_product:
+        number = sum([pow(i, 2) for i in j])
+        max_number = max(max_number, number % M)
+
+    print(max_number)
 
 list_product = list(product(*my_list))
 
