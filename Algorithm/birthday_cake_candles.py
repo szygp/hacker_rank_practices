608297ffa15be1cec6e782fa3aaed0037892329e
sophie.zhang <agdzyg@gmail.com>
2023-01-31 20:51:40 +0800

    birthday_cake_candles

---
+++ Algorithm/birthday_cake_candles.py
@@ -0,0 +1,35 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'birthdayCakeCandles' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY candles as parameter.
+#
+
+def birthdayCakeCandles(candles):
+    record = 0
+    max_c = max(candles)
+    for i in candles:
+        if i == max_c:
+            record += 1
+    return record
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
